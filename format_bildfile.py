e01a08f4fe81172812f990b4d8d45acaf3b602b2
Matthew Iadanza <fbsmi@absl-wkstn01.leeds.ac.uk>
2020-01-29 12:54:42 +0000

    added more flexibility to change axis and path separately

--- format_bildfile.py
+++ format_bildfile.py
@@ -8,15 +8,18 @@ import sys
 try:
     bildfile = open(sys.argv[1],'r').readlines()
     cyl_size = float(sys.argv[2])
-    sphere_size = float(sys.argv[3])
+    axis_size = float(sys.argv[3])
+    sphere_size = float(sys.argv[4])
 except:
-    sys.exit('USAGE: bildfile_figure.py <bild file> <cylinder size> <sphere size>')
+    sys.exit('USAGE: bildfile_figure.py <bild file> <movement cylinder size> <axis cylinder size> <sphere size>\n use axis cylinder size of 0 to leave the axis markers as vectors')
 
 output = open('FQ_{0}'.format(sys.argv[1].split('/')[-1]),'w')
 for i in bildfile:
     line = i.split()
-    if line[0] == '.v':
-        line = '.cylinder {0} {1}'.format(' '.join(line[1:]),cyl_size)
+    if line[0] == '.cylinder':
+        line = '.cylinder {0} {1}'.format(' '.join(line[1:-1]),cyl_size)
+    elif line[0] == '.v' and axis_size !=0:
+        line = '.cylinder {0} {1}'.format(' '.join(line[1:]),axis_size)
     elif line[0] == '.sphere':
         line = '{0} {1}'.format(' '.join(line[:-1]),sphere_size)
     else:
